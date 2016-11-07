66155dfb7007d7f5f7972ca74e3cc199eb4ebc3c
John Shen <johnzhihaoshen@gmail.com>
2016-11-07 12:57:44 -0800

    start_game() works, added pseudocode

--- naive_score.py
+++ naive_score.py
@@ -1,4 +1,5 @@
 import json
+import string
 import requests
 
 start_url = 'http://jswidler.com/challenge'
@@ -34,25 +35,45 @@ guess_url = 'http://jswidler.com/challenge/guess'
 # 	  "guessesLeft": 4999
 # }
 
-solverName = 'jzebra_00'
+solver_name = 'jzebra_naive_00'
 # setting these as global variables to make things easier. 
 # pass these as params to guess() when we want to clean it up.
 token = None
-secretLength = None
+secret_length = None
+last_guess = None
 
 def start_game():
 	params = {
-		'solverName': solverName
+		'solverName': solver_name
 	}
 
-	response = requests..post(start_url, params=params)
+	response = requests.post(start_url, params=params)
 	global token
-	global secretLength
-	token = str(response.get('token'))
-	secretLength = int(response.get('secretLength'))
-
-def guess():
-
-
+	global secret_length
+	token = str(response.json().get('token'))
+	secret_length = int(response.json().get('startingSecretLength')) 
 
 def run():
+	start_game()
+	print token
+	print secret_length
+
+# part 1: building the score map for each position
+# treat each letter in the guess as a separate problem
+# change the letter
+# concat many letters to make a guess
+# submit the new guess
+# each position in the guess has a letter: score map. if numberLettersCorrect increases, increment the score 
+# repeat changing the letter until we reach z OR the total numberLettersCorrect for this level == secretLength
+# 
+# part 2: guessing from the map
+# using the highest scoring letters for each position, make a guess
+# change one position
+# if score decreases, undo the change
+# if score increases, lock in the change
+# else, repeat
+
+
+
+if __name__ == "__main__":
+	run()
